b2cb09b4de53123c96836d6aa347217a80c3a56f
Jacobo Garcia <jacobo0312@Jacobos-MacBook-Air.local>
2023-01-30 11:45:40 -0500

    Add prettier file

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  semi: false,
+  tabWidth: 2,
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'none'
+}
